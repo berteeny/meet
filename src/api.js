6db2429c9d351abcef7cd9d8f12d93e05bb01e0b
berteeny <aegbertoni@gmail.com>
2024-07-23 14:42:54 -0700

    removed const for removeQuery()

--- src/api.js
+++ src/api.js
@@ -6,6 +6,27 @@ export const extractLocations = (events) => {
   return locations;
 };
 
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem("access_token");
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        "https://cj8355uti2.execute-api.ca-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getAccessToken(code);
+  }
+  return accessToken;
+};
+
 export const checkToken = async (accessToken) => {
   const response = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
@@ -27,7 +48,7 @@ export const getToken = async (code) => {
   return access_token;
 };
 
-export const removeQuery = () => {
+const removeQuery = () => {
   let newurl;
   if (window.history.pushState && window.location.pathname) {
     newurl =
@@ -61,25 +82,4 @@ export const getEvents = async () => {
       return result.events;
     } else return null;
   }
-};
-
-export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem("access_token");
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const response = await fetch(
-        "https://cj8355uti2.execute-api.ca-central-1.amazonaws.com/dev/api/get-auth-url"
-      );
-      const result = await response.json();
-      const { authUrl } = result;
-      return (window.location.href = authUrl);
-    }
-    return code && getAccessToken(code);
-  }
-  return accessToken;
 };
